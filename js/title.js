23f1eede2eece335c0d4e491a726f6d02646a141
Harun Abdullah Rakin <160221616+harunabdullahrakin@users.noreply.github.com>
2024-06-18 18:47:39 +0600

    Add files via upload

---
+++ js/title.js
@@ -0,0 +1,23 @@
+var rev = "fwd";
+function titlebar(val){
+    var msg  = "Harun Abdullah Rakin";
+    var res = " ";
+    var speed = 450;
+    var pos = val;
+    msg = msg;
+    var le = msg.length;
+    if(rev == "fwd"){
+        if(pos < le){
+            pos = pos+1;
+            scroll = msg.substr(0,pos);
+            document.title = scroll;
+            timer = window.setTimeout("titlebar("+pos+")",speed);
+        }
+        else {
+            rev = "bwd";
+            timer = window.setTimeout("titlebar("+pos+")",speed);
+        }
+    }
+
+}
+titlebar(0);
